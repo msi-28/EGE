7e4338983fa1338e9e29a4e38f0ee2101938665c
MVarfolomeeva <mrnvflmv2@gmail.com>
2024-12-24 17:06:33 +0200

    задание 5 №19237 (ЕГКР) 24.12.2024

--- ЕГКР-21-12-24/task-5-19237.py
+++ ЕГКР-21-12-24/task-5-19237.py
@@ -1,3 +1,18 @@
-print(123)
+def convert(num, q):
+    ans = ''
+    while num:
+        ans += str(num%q)
+        num //= q
+    return ans[::-1]
 
-print(456)
+ls = []
+for N in range(1, 10_000):
+    R = convert(N, 3)
+    if N%3 == 0:
+        R = R + R[-2:]
+    else:
+        R = R + convert(sum(map(int, R)), 3)
+    R = int(R, 3)
+    if R > 220 and R % 2 == 0:
+        ls.append(R)
+print(min(ls))
