b94acde30217af748d0b9c588ca430d2ecc509ff
MVarfolomeeva <mrnvflmv2@gmail.com>
2025-01-07 17:28:43 +0200

    №14327 (вместо собстенной функции лучше использовать встроенную (зд. oct))

--- 07-01-25/task-5-14327.py
+++ 07-01-25/task-5-14327.py
@@ -5,15 +5,14 @@ def convert(num, q):
         num //= q
     return ans[::-1]
 
-ans = 0
-for N in range(9, 10):
-    R = convert(N, 8)
+max_N = 0
+for N in range(1, 10_000):
+    R = oct(N)[2:]
     if N % 2 == 0:
         R = R + max(R)
     else:
-        R = R + convert(int(min(R)) * 2, 8)
+        R = R + oct(int(min(R)) * 2)[2:]
     R = int(R, 8)
-    print(N, R)
-#     if R < 313:
-#         ans = N
-# print(ans)
+    if R < 313:
+        max_N = N
+print(max_N)
