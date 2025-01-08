9b661a87b71ccfbdc5dfc83df84958d91b87323a
msi-28 <mrnvflmv2@gmail.com>
2025-01-08 18:48:22 +0200

    Задание 24. Ответ: 123

---
+++ krilov-1-variant/task-24-var1/task-24-var1.py
@@ -0,0 +1,15 @@
+with open('24var01.txt') as file:
+    st = file.readline()
+
+st = st.replace('-', '+')
+
+while '++' in st:
+    st = st.replace('++', '+ +')
+st = st.replace('+0', 'x')
+while 'x0' in st:
+    st = st.replace('x0', 'x')
+st = st.replace('x', ' ')
+st = st.replace(' +', ' ').replace('+ ', ' ')
+st = st.split()
+
+print(len(max(st, key=len)))
