62d3b4881367236083e88d855fd7b4c1a63b4af7
msi-28 <mrnvflmv2@gmail.com>
2025-01-08 19:23:32 +0200

    Задание 17. Ответ: 704 197847

--- krilov-1-variant/task-17-var1/task-17-var1.py
+++ krilov-1-variant/task-17-var1/task-17-var1.py
@@ -5,11 +5,14 @@ cnt = 0
 ans = -10
 for i in range(len(ls) - 1):
     sum = ls[i] + ls[i+1]
-    c1 = ls[i] % 27 == min(ls[i], ls[i + 1]) or ls[i + 1] % 27 == min(ls[i], ls[i + 1])
+    c1 = ls[i] % 27 == min(ls) or ls[i + 1] % 27 == min(ls)
     if c1:
         cnt += 1
         if sum > ans:
             ans = sum
 print(cnt, ans)
 
-# 6 99107 <- ответ
+# 6 99107 <- ответ(-)
+
+# Примечание: (последовательность = весь список) => минимальный элемент последовательности = min(ls)
+# ответ: 704 197847
