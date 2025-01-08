d50290e134714ac72cd6b84daca43fd57dbc2f95
msi-28 <mrnvflmv2@gmail.com>
2025-01-08 16:36:22 +0200

    Задание 6. Ответ: 34

---
+++ krilov-1-variant/task-6-var1.py
@@ -0,0 +1,31 @@
+from turtle import *
+
+tracer(0)
+lt(90)
+m = 7
+
+for i in range(5):
+    fd(30 * m)
+    rt(90)
+    fd(40 * m)
+    rt(90)
+
+up()
+fd(20 * m)
+rt(90)
+fd(5 * m)
+rt(90)
+down()
+
+for i in range(7):
+    fd(10 * m)
+    rt(90)
+
+up()
+for x in range(30, 60):
+    for y in range(10, 60):
+        goto(x * m, y * m)
+        dot(3, 'red')
+done()
+
+# (6 + 11)*2 = 34 <- ответ
