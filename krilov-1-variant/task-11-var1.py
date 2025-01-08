6b9232293758e009f7cc9367e418724f9060696c
msi-28 <mrnvflmv2@gmail.com>
2025-01-08 17:23:46 +0200

    Задание 14. Ответ: 5768

---
+++ krilov-1-variant/task-11-var1.py
@@ -0,0 +1,11 @@
+ans = 0
+for x in range(1, 5769):
+    num = 9**2025 + 9**1000 - x
+    cnt = 0
+    while num:
+        if num % 9 == 0:
+            cnt += 1
+        num //= 9
+    if cnt == 1026:
+        ans = x
+print(ans)
