a581e311218ed6324f9d7b4e3910030eb105be05
MVarfolomeeva <mrnvflmv2@gmail.com>
2025-01-10 16:22:42 +0200

    ans -> 20736

--- prob-09-01-25/task-8.py
+++ prob-09-01-25/task-8.py
@@ -4,10 +4,10 @@ alf = 'ABCDEF'
 cnt = 0
 for s in product(alf, repeat=6):
     s = ''.join(s)
-    c1 = s[0] != 'A'
-    c2 = s[-1] != 'A'
+    c1 = s[0] != 'A' and s[0] != 'E'
+    c2 = s[-1] != 'A' and s[-1] != 'E'
     if c1 and c2:
         cnt += 1
 print(cnt)
 
-# ans -> 32400
+# ans -> 20736
