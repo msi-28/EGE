900ff90913710b6f2439632c65d5843a30d34d30
MVarfolomeeva <mrnvflmv2@gmail.com>
2025-01-10 17:09:00 +0200

    правильное решение 23 задание + то, которое было (исключене числа)

--- prob-09-01-25/task-23.py
+++ prob-09-01-25/task-23.py
@@ -4,5 +4,11 @@ def f(s,e):
     return f(s + 2, e) + f(s * 3, e) + f(s * 2, e)
 
 print(f(13, 15) * f(17, 45))
+
+# правильное решение
+def f(s,e):
+    if s > e or s == 16: return 0
+    if s == e: return 1
+    return f(s + 2, e) + f(s * 3, e) + f(s * 2, e)
+
 print(f(13, 45))
-print(f(13, 16), f(16, 45))
