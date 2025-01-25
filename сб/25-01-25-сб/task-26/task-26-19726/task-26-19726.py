30495ddba0f3f92d310b32292afcaaaee83a13c0
MVarfolomeeva <mrnvflmv2@gmail.com>
2025-01-25 17:34:31 +0200

    gg

--- сб/25-01-25-сб/task-26/task-26-19726/task-26-19726.py
+++ сб/25-01-25-сб/task-26/task-26-19726/task-26-19726.py
@@ -6,8 +6,9 @@ with open('26.20_19726.txt') as file:
         meetings.append([start, end, end - start])
 
 meetings = sorted(meetings, key= lambda x: (x[1], x[2]))
-print(meetings)
 
 approved = [meetings[0]]
-# for meet in meetings:
-#     if approved[]
+for meet in meetings:
+    if approved[-1][1] <= meet[0]:
+        approved.append(meet)
+print(len(approved), approved[-1][1])
