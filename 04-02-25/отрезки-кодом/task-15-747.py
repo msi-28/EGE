5c767676d2f8be7273fd4b9b855758733d6e9d91
msi-28 <mrnvflmv2@gmail.com>
2025-02-06 19:18:17 +0200

    отрезки кодом

---
+++ 04-02-25/отрезки-кодом/task-15-747.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+def f(x):
+    P = 43 <= x <= 49
+    Q = 44 <= x <= 53
+    A = A1 <= x <= A2
+    return (A <= P) or Q
+ans = []
+line = [x/10 for x in range(40*10, 55*10)]
+for A1, A2 in combinations(line, 2):
+    if all(f(x) for x in line):
+        ans.append(A2 - A1)
+
+print(max(ans))
