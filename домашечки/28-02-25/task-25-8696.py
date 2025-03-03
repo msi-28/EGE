b66b89931ebd5bac5b0b7411fffb81cb666ddbaf
msi-28 <mrnvflmv2@gmail.com>
2025-03-03 21:18:52 +0200

    25_8696 Делители числа
    Ответ:
    1273566 1637537
    1273570 1139869
    1273578 1287317
    1273582 651769
    1273590 2225609
    
    https://kompege.ru/variant?kim=25085347

---
+++ домашечки/28-02-25/task-25-8696.py
@@ -0,0 +1,21 @@
+def f(x):
+    ans = set()
+    for i in range(2, int(x**0.5) + 1):
+        if x%i == 0:
+            ans |= {i, x//i}
+    return sum(ans)
+
+cnt = 0
+for i in range(1_273_547, 2_000_000):
+    if f(f(i)%100_000) == 0 and f(i) != 0:
+        print(i, f(i))
+        cnt += 1
+        if cnt == 5:
+            break
+
+# ans:
+# 1273566 1637537
+# 1273570 1139869
+# 1273578 1287317
+# 1273582 651769
+# 1273590 2225609
