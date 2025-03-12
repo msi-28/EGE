82a4b5207e1be512f6e953ab6d93b7ca9e652467
msi-28 <mrnvflmv2@gmail.com>
2025-03-12 17:59:48 +0200

    16_19709
    Ответ: 24874421616
    https://kompege.ru/variant?kim=25087422

---
+++ домашечки/11-03-25/task-16-19709.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+
+@lru_cache(None)
+def f(n):
+    if n == 1: return 1
+    if n > 1: return n**3 + f(n - 1)
+
+for i in range(2026):
+    f(i)
+print(f(2025) - f(2022))
+
+# asn -> 24874421616
