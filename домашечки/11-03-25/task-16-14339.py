e6fa4356c3483226517240feab5eb7cea663a6b0
msi-28 <mrnvflmv2@gmail.com>
2025-03-12 18:05:11 +0200

    16_14339
    Ответ: 16492
    https://kompege.ru/variant?kim=25087422

---
+++ домашечки/11-03-25/task-16-14339.py
@@ -0,0 +1,14 @@
+from functools import lru_cache
+
+@lru_cache(None)
+def f(n):
+    if n < 11: return n
+    if n >= 11 and n%2 == 0: return 2*n - 3 + f(n - 2)
+    if n >= 11 and n%2 != 0: return 3*n - 4 + f(n - 3)
+
+for i in range(5501):
+    f(i)
+
+print(f(5500) - f(5497))
+
+# ans -> 16492
