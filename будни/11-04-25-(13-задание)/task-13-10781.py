00e3b8cf624fc2e3b69c1f9c866b56be5a31b94d
MVarfolomeeva <mrnvflmv2@gmail.com>
2025-04-11 17:50:14 +0200

    13 (10781)
    ответ: 8192
    https://kompege.ru/variant?kim=25093899

--- будни/11-04-25-(13-задание)/task-13-10781.py
+++ будни/11-04-25-(13-задание)/task-13-10781.py
@@ -5,6 +5,7 @@ for mask in range(33):
     net2 = ip_network(f'112.117.121.80/{mask}', 0)
     if net1.network_address == net2.network_address:
         print(net2.netmask)
+        # print(2 ** (32 - mask))
 
 print(bin(224))
 print(2**len('00000' + '0'*8))
