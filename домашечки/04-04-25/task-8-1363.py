3189e0f446bb7f949e07a9a666b8ad529399d421
msi-28 <mrnvflmv2@gmail.com>
2025-04-18 13:47:48 +0200

    8 задание пробник дз ответ: 1562 https://kompege.ru/variant?kim=25093291

--- домашечки/04-04-25/task-8-1363.py
+++ домашечки/04-04-25/task-8-1363.py
@@ -3,9 +3,10 @@ from itertools import *
 cnt = 0
 for s in product('01234', repeat=6):
     s = ''.join(s)
-    c0 = s[0] != '0'
-    c1 = s[1] == '3'
-    if c0 and c1:
+    c1 = s[0] == '3'
+    c2 = int(s, 5)%2 == 0
+    if c1 and c2:
         cnt += 1
+        print(s)
 print(cnt)
-# ans -> 2500
+# ans -> 1562
