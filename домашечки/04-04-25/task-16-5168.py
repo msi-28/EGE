2043ca7d7c4761c00286c7f9ffe9547331f10181
msi-28 <mrnvflmv2@gmail.com>
2025-04-18 14:20:58 +0200

    16 задание пробник дз ответ: 9341 https://kompege.ru/variant?kim=25093291

--- домашечки/04-04-25/task-16-5168.py
+++ домашечки/04-04-25/task-16-5168.py
@@ -6,6 +6,8 @@ def f(n):
     if n < 10_000 and n%2 == 0: return f(n+1) + n**2 - 3*(n - 1)
     if n < 10_000 and n%2 != 0: return f(n+2) + 5*n - (n - 1)
 
-for i in range(1, 10_001):
+for i in range(10_000, 0, -1):
     f(i)
 print(f(90) - f(99))
+
+# ans -> 9341
