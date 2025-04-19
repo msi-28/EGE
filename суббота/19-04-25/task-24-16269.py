f8c318bd3e8824517a7bc8ed1c9404b2489e005b
MVarfolomeeva <mrnvflmv2@gmail.com>
2025-04-19 18:08:31 +0200

    24 (16269) регулярки (через finditer)
    ответ: 52
    пофикшено через индексы

--- суббота/19-04-25/task-24-16269.py
+++ суббота/19-04-25/task-24-16269.py
@@ -1,16 +1,15 @@
 from re import *
 from itertools import product
 
-st = open('24_16269.txt').readline()
-# st = 'YYZZZZYYXX'
-pattern = rf'((?<!XX)(XX)(?!XX)*|(?<!YY)(YY)(?!YY)*|(?<!ZZ)(ZZ)(?!ZZ)*)+'
+st = ')))' + open('24_16269.txt').readline() + '))))'
+# st = ')))' +'YYZZZZYYXX'+ '))))'
+pattern = r'((?<!XX)(XX)(?!XX)*|(?<!YY)(YY)(?!YY)*|(?<!ZZ)(ZZ)(?!ZZ)*)+'
 
 m = finditer(pattern, st)
 ans = []
 for i in m:
     if i.group()!= '':
-        ans.append((i.group()))
-# print(eval(max(ans, key=eval)))
-print(len(max(ans, key=len)))
-print(ans)
-# ans -> 50 но должно быть 52
+        ans.append(st[i.start()-3:i.end()+3])
+print((max(ans, key=len)))
+print(len('YYXXZZXXYYZZXXYYZZXXZZYYXXYYXXYYXXYYZZXXYYXXYYZZYYZZ'))
+# ans -> 52
