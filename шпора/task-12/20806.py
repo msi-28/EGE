8ad8a8242210c336e9d233cf793143c9de78f968
MVarfolomeeva <mrnvflmv2@gmail.com>
2025-06-04 16:01:08 +0200

    дз 05.06.25. ответ: 88

---
+++ шпора/task-12/20806.py
@@ -0,0 +1,8 @@
+st = '1'*81
+while '111' in st or '88888' in st:
+    if '111' in st:
+        st = st.replace('111', '88',1)
+    else:
+        st = st.replace('88888', '8',1)
+print(st)
+# ans -> 88
